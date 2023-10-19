c74c68e68f4a0c0d36097b6177e24bf9b7122fcd
cabrel <jeancode1234@gmail.com>
2023-10-19 16:18:06 +0100

    third commit

--- authentification/login.php
+++ authentification/login.php
@@ -3,9 +3,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href=".././bootstrap/css/bootstrap.min.css">
     <title>Login - Page</title>
 </head>
 <body>
-    
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="row">
+                    <div class="col-md-6">
+                        <form action="" method="post">
+                            <div class="form-group">
+                                <label for="matricule">Matricule</label>
+                                <input type="text" name="matricule" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="password">Mot de passe</label>
+                                <input type="password" name="password" class="form-control">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
